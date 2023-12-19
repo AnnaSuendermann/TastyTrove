4d21417322bff9cab4fd373d2b8891c325102833
AnnaSuendermann <123335297+AnnaSuendermann@users.noreply.github.com>
2023-12-19 09:52:57 +0100

    Categorie.java aktualisieren

--- src/main/java/com/sa/tastytrove/entity/Categorie.java
+++ src/main/java/com/sa/tastytrove/entity/Categorie.java
@@ -1,3 +1,36 @@
+package com.sa.tastytrove.entity;				
+				
+import jakarta.persistence.Column;				
+import jakarta.persistence.Entity;				
+import jakarta.persistence.Id;				
+import jakarta.persistence.Table;	
+
+
+@Entity
+@Table(name= "Categories")
 public class Categorie {
-    
+	@Id
+	@Column(name= "CategoryID")
+	private Long id;
+
+	@Column(name= "Name")
+	private String name;
+
+	public void setCategoryId(Long id){
+		this.id = id;
+	}   
+
+	public Long getCategoryId(){
+		return id;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+
 }
