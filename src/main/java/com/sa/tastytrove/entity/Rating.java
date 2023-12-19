d4f248fa5c4bfe8762b6bfab1ba43bd82bfdc976
AnnaSuendermann <123335297+AnnaSuendermann@users.noreply.github.com>
2023-12-19 09:46:30 +0100

    Rating.java aktualisieren

--- src/main/java/com/sa/tastytrove/entity/Rating.java
+++ src/main/java/com/sa/tastytrove/entity/Rating.java
@@ -1,3 +1,40 @@
+package com.sa.tastytrove.entity;				
+				
+import jakarta.persistence.Column;				
+import jakarta.persistence.Entity;				
+import jakarta.persistence.Id;				
+import jakarta.persistence.Table;	
+
+@Entity
+@Table(name= "Ratings")
+
+// TODO check names and add connection to other tables
+
 public class Rating {
-    
+	@Id
+	@Column(name= "RatingID")
+	private Long id;
+
+	@Column(name= "RecipeID")
+	private Long id;
+
+	@Column(name= "Satisfaction")
+	private int rating;
+
+	public Long getRatingID(){
+
+	}
+
+	public Long getRecipeID(){
+
+	}
+	
+	public void setSatisfaction(int rating){
+		this.rating = rating;
+	}
+	
+	public int getSatisfaction(){
+		return rating;
+	}	
+
 }
