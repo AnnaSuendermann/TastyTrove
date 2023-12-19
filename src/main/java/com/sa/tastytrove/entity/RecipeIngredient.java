448d8a975842dbcbd6addb5ec452ca903d3cd521
AnnaSuendermann <123335297+AnnaSuendermann@users.noreply.github.com>
2023-12-19 09:29:26 +0100

    RecipeIngredient.java aktualisieren

--- src/main/java/com/sa/tastytrove/entity/RecipeIngredient.java
+++ src/main/java/com/sa/tastytrove/entity/RecipeIngredient.java
@@ -1,3 +1,24 @@
-public class RecipeIngredient {
-  
-}
+package com.sa.tastytrove.entity;				
+				
+import jakarta.persistence.Column;				
+import jakarta.persistence.Entity;				
+import jakarta.persistence.Id;				
+import jakarta.persistence.Table;				
+				
+@Entity				
+// TODO check name and add information for table interaction later				
+@Table(name = "RecipeIngredients")				
+public class RecipeIngredient{				
+	@Id			
+	@Column(name = "RecipeID")			
+	private Long id;			
+				
+	@Column(name = "IngredientID")			
+	private Long id;			
+				
+	@Column(name = "Amount")			
+	private Long id;			
+	
+
+			
+				
